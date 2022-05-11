e32ca03c272a83427bfc62be392c31bf08587394
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-05-10 22:32:44 -0500

    update week 6

--- 5-semana/2_ghost_e2e_kraken/features/web/step_definitions/step.js
+++ 5-semana/2_ghost_e2e_kraken/features/web/step_definitions/step.js
@@ -1,4 +1,5 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
+const expect = require('chai').expect;
 
 When("I enter email {kraken-string}", async function (email) {
   let element = await this.driver.$("#ember7");
@@ -12,7 +13,7 @@ When("I enter password {kraken-string}", async function (password) {
   return await element.setValue(password);
 });
 
-When("I click button", async function () {
+When("I click button loggin", async function () {
   let element = await this.driver.$("#ember12");
 
   return await element.click();
@@ -48,6 +49,11 @@ When('I go to pages', async function () {
   return await element.click();
 });
 
+When('I click on publish link', async function () {
+  let element = await this.driver.$('.ember-view.ember-basic-dropdown-trigger ember-basic-dropdown-trigger--right.ember-basic-dropdown-trigger--below.gh-btn gh-btn-outline.gh-publishmenu-trigger')
+  return await element.click();
+})
+
 When('I go to tags', async function () {
   let element = await this.driver.$('a[href*="tags"]');
   return await element.click();
@@ -58,7 +64,17 @@ When('I click on new tag', async function () {
   return await element.click();
 });
 
+When('I type a tag title', async function () {
+  let element = await this.driver.$('input[id="tags-name"]');
+  return await element.type('New tag title');
+});
+
 When('I click on save tag', async function () {
   let element = await this.driver.$('.gh-btn.gh-btn-blue.gh-btn-icon.ember-view');
   return await element.click();
 });
+
+Then('I expect an error message', async function () {
+  let elements = await this.driver.$$('.gh-btn.gh-btn-blue.gh-btn-icon.gh-btn-red.ember-view');
+  expect(elements.length).to.equal(1);
+})
